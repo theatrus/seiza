ca86859232b06712a83a655c53ebbbe481731004
Yann Ramin <github@theatr.us>
2026-07-21 13:01:54 -0700

    Format Python stacking imports

--- seiza-py/src/stacking.rs
+++ seiza-py/src/stacking.rs
@@ -4,9 +4,9 @@ use pyo3::exceptions::{PyRuntimeError, PyValueError};
 use pyo3::prelude::*;
 use seiza_stacking::{
     CalibrationMasters, DeltaSigmaOptions, FitsFrame, FrameDisposition, LinearImage, LiveStacker,
-    MasterBuildOptions, MasterDark, MasterFrameKind, MasterRejectionOptions,
-    NormalizationMode, RejectionMode, StackOptions, StackSnapshot, build_master_from_fits,
-    paths_refer_to_same_file, write_fits_f32, write_master_fits_f32,
+    MasterBuildOptions, MasterDark, MasterFrameKind, MasterRejectionOptions, NormalizationMode,
+    RejectionMode, StackOptions, StackSnapshot, build_master_from_fits, paths_refer_to_same_file,
+    write_fits_f32, write_master_fits_f32,
 };
 use std::path::{Path, PathBuf};
 
